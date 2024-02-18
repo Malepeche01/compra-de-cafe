932afcd98234da4ebf420445486ca1be608760de
Malena Pchersky <malena.pechersky@gmail.com>
2024-02-18 17:26:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+const container = document.getElementById("content");
+container.classList.add("container");
+
+
+const titulo = document.createElement("h2");
+titulo.innerText = ("Café de Especialidad en Casa");
+container.appendChild(titulo);
+let carrito = []
+
+
+
+
+
+fetch("./js/variedades.json")
+    .then(response => response.json())
+    .then(data => {
+ 
+    data.forEach(variedad => {
+        const card = document.createElement("card");
+        card.classList.add("card");
+    
+        const nombreVariedad = document.createElement("h4")
+        nombreVariedad.innerText =  variedad.nombre;
+
+        const perfilDeTueste = document.createElement("p")
+        perfilDeTueste.innerText = variedad.perfilDeTueste;
+    
+    
+        const imgVariedad = document.createElement("img")
+        imgVariedad.src = variedad.imagen;
+    
+    
+        const precioVariedad = document.createElement("p")
+        precioVariedad.innerText = `$ ${variedad.precio}`
+        
+        let botonAgregar = document.createElement("button")
+        botonAgregar.innerText = "Agregar"
+    
+        
+
+        botonAgregar.addEventListener("click", () => {
+        carrito.push({
+            id: variedad.id,
+            imagen: variedad.imagen,
+            precio: variedad.precio,
+            })
+
+        console.log(carrito);
+        guardar()
+        })
+    
+
+        card.appendChild(nombreVariedad);
+        card.appendChild(perfilDeTueste);
+        card.appendChild(imgVariedad);
+        card.appendChild(precioVariedad);
+        card.appendChild(botonAgregar);  
+        container.appendChild(card);  
+    })
+})
+
+verCompra.addEventListener("click", () =>{
+    const tituloCarrito = document.createElement("div")
+    tituloCarrito.innerHTML = `
+    <h2>Carrito</h2>`
+    
+    contenidoCarrito.append(tituloCarrito);
+
+    
+    carrito.forEach((el) => {
+    
+        let miCarrito = document.createElement("div")
+        miCarrito.innerHTML = `
+        <img src="${el.imagen}">
+        <h4> ${el.id}</h4>
+        <p> $${el.precio}<p/>
+
+        `
+        
+        contenidoCarrito.append(miCarrito);
+    
+        const total = carrito.reduce((acc,el) => acc + el.precio, 0);
+        const totalCompra = document.createElement("div");
+        totalCompra.innerHTML = `Su compra: $${total}`;
+
+        contenidoCarrito.append(totalCompra);
+    })
+
+})
+
+
+
+const guardar = () => {
+    localStorage.setItem("miCarrito", JSON.stringify(carrito))
+    }
+
+
+
+
+        
+
+
